fc4f7e6adc9b9a718d564c0d422bbb1f80420c34
Tom-Shumi <tshumiya281@gmail.com>
2021-03-08 22:31:45 +0900

    add: 認証処理途中

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import {Container, Button, Form} from 'react-bootstrap';
 import styles from '../styles/Index.module.css';
 import axios from "axios";
 
 const index: React.FC = () => {
+
+    const [token, setToken] = useState<string>("");
+    const [username, setUsername] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+
+    const login = () =>{
+
+    }
+
     return (
         <div className="form_frame">
             <Container>
@@ -12,12 +21,12 @@ const index: React.FC = () => {
                     <p className={styles.login_str}>Login</p>
                     <hr />
                     <Form.Label>User Name</Form.Label>
-                    <Form.Control type="text" placeholder="User Name" />
+                    <Form.Control type="text" placeholder="User Name" onChange={(e: any) => setUsername(e.target.value)} value={username} />
                     <br />
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control type="password" placeholder="Password" onChange={(e: any) => setPassword(e.target.value)} value={password} />
                     <br />
-                    <Button variant="outline-primary" size="lg" block>Login</Button>
+                    <Button variant="outline-primary" size="lg" block onClick={login}>Login</Button>
                 </Form>
             </Container>
             <hr />
