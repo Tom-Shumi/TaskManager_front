ddb775c17ac9de115f7c0b86f8e00f5c34480294
Tom-Shumi <tshumiya281@gmail.com>
2021-03-31 22:58:45 +0900

    add: タスクのDrag&Drop処理途中

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,7 +1,10 @@
 import React , { Dispatch, SetStateAction } from 'react';
 import TaskItem from '../components/TaskItem';
 import styles from '../styles/TaskList.module.css';
-import { Task } from './interface';
+import { ItemTypes, Task } from './interface';
+import { useDrop } from 'react-dnd';
+import Axios from "axios";
+import Router from 'next/router';
 
 interface TaskListProps {
     taskList: Task[];
@@ -11,9 +14,32 @@ interface TaskListProps {
 }
 
 const TaskList: React.FC<TaskListProps> = (props) => {
+
+    // cookieを使用するaxios生成
+    let client = Axios.create({ withCredentials: true });
+
+    const updateTaskStatus = (e) => {
+        client.put(process.env.NEXT_PUBLIC_API_SERVER + process.env.NEXT_PUBLIC_API_TASK + "/" + "")
+        .then( response => {
+            props.setInitDispFlg(true);
+        }).catch(() => {
+            Router.push('/Error?400');
+        })
+        e.stopPropagation();
+    }
+
+    const [{isOver}, drop] = useDrop({
+        accept: ItemTypes.TASK_ITEM,
+        drop: () => console.log("drop"),
+        collect: monitor => ({
+            isOver: !!monitor.isOver()
+        })
+    })
+
     var status_str = conversionStatus(props.status);
+    var style_is_over = isOver ? "is_over" : "is_not_over"; 
     return (
-        <div className={styles.task_list}>
+        <div className={styles.task_list + " " +  style_is_over} ref={drop}>
             <p className={styles.task_status}>{status_str} [{Object.keys(props.taskList).length}]</p>
             {
                 props.taskList.map(task_item => (
