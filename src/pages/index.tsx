677858f58f33a902d0dbe16adeb67cda30821b6e
Tom-Shumi <tshumiya281@gmail.com>
2021-07-11 14:53:21 +0900

    Add create User execute processs from frontend

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,17 +16,9 @@ const index: React.FC = () => {
         if (username == "" || password == "") {
             return false;
         }
-        postToken();
-    }
 
-    // ログインapi実行
-    const postToken = () => {
-        var params = new URLSearchParams();
-        params.append('username', username);
-        params.append('password', password);
-
-        client.post(`${process.env.NEXT_PUBLIC_API_SERVER + process.env.NEXT_PUBLIC_API_LOGIN}`, params)
-        .then( response => {
+        client.post(`${process.env.NEXT_PUBLIC_API_SERVER + process.env.NEXT_PUBLIC_API_LOGIN}`, createParams())
+        .then(() => {
             console.log('login success');
             // セッションにログイン情報保持
             sessionStorage.clear();
@@ -34,16 +26,53 @@ const index: React.FC = () => {
 
             Router.push('/Task');
 
-        }).catch(() => {
+        }).catch((err) => {
+            console.log('err:', err.response);
             Router.push('/Error?401');
         })
+    }
+
+    const createUser = () => {
+        if (username == "" || password == "") {
+            return false;
+        }
+
+        client.post(`${process.env.NEXT_PUBLIC_API_SERVER + process.env.NEXT_PUBLIC_API_USER_NOAUTH}`
+                    , createJsonParams()
+                    , {headers: {'content-type': 'application/json'}})
+        .then(() => {
+            console.log('Success to create user');
+            login()
+        }).catch((err) => {
+            console.log('err:', err.response);
+            alert(err.response.data);
+        })
+    }
+
+    const createParams = () => {
+        var params = new URLSearchParams();
+        params.append('username', username);
+        params.append('password', password);
+
+        return params;
+    }
+
+    const createJsonParams = () => {
+        var params = {
+            username: username,
+            password: password,
+            enabledflg: 1,
+            adminflg: 0
+        }
+        
+        return JSON.stringify(params);
     }
 
     return (
         <div className="form_frame">
             <Container>
                 <Form>
-                    <p className={styles.login_str}>Login</p>
+                    <p className={styles.login_str}>Task manager</p>
                     <hr />
                     <Form.Label>User Name</Form.Label>
                     <Form.Control type="text" placeholder="User Name" onChange={(e: any) => setUsername(e.target.value)} value={username} />
@@ -52,6 +81,9 @@ const index: React.FC = () => {
                     <Form.Control type="password" placeholder="Password" onChange={(e: any) => setPassword(e.target.value)} value={password} />
                     <br />
                     <Button variant="outline-primary" size="lg" block onClick={login}>Login</Button>
+                    <br />
+                    <br />
+                    <Button variant="outline-success" size="lg" block onClick={createUser}>Create User</Button>
                 </Form>
             </Container>
             <hr />
