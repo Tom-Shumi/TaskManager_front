6491fce88aed2d50ac27dc7338326879ff9851c0
Tom-Shumi <tshumiya281@gmail.com>
2021-08-12 23:22:02 +0900

    fix gitignore

---
+++ .next/static/development/_buildManifest 3.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/DailyTask":["static\u002Fchunks\u002Fpages\u002FDailyTask.js"],"/DailyTaskHistory":["static\u002Fchunks\u002Fpages\u002FDailyTaskHistory.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],sortedPages:["\u002FDailyTask","\u002FDailyTaskHistory","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
