3d66736a726a85987137c4d40113c3b25e61aa12
Tom-Shumi <tshumiya281@gmail.com>
2021-10-30 10:14:06 +0900

    del unused import

--- src/pages/DailyTaskChangeOrder.tsx
+++ src/pages/DailyTaskChangeOrder.tsx
@@ -2,7 +2,6 @@ import Layout from 'components/common/Layout';
 import {Button} from 'react-bootstrap';
 import Link from 'next/link';
 import DailyTaskList from 'components/DailyTaskChangeOrder/DailyTaskList';
-import dynamic from "next/dynamic";
 import { DndProvider } from 'react-dnd';
 import {HTML5Backend} from 'react-dnd-html5-backend';
 
