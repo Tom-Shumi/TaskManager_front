cb306560699eb49fa106597a4e0434125564b827
Tom-Shumi <tshumiya281@gmail.com>
2021-11-04 22:28:56 +0900

    add Theme Edit form

--- src/components/ZeroSecondThinkingList/Item.tsx
+++ src/components/ZeroSecondThinkingList/Item.tsx
@@ -5,15 +5,39 @@ import Content from 'components/ZeroSecondThinkingList/Content';
 
 const Item: React.FC = () => {
   const [isOpen, setIsOpen] = useState<Boolean>(false);
+  const [isEdit, setIsEdit] = useState<Boolean>(false);
+  const [theme, setTheme] = useState<string>("なぜ0秒思考を毎日やるのか？Edit");
 
   const openContent = () => {
     setIsOpen((isOpen) => !isOpen);
   }
 
+  const editTheme = (e: any) => {
+    setIsEdit((isEdit) => !isEdit);
+    e.stopPropagation();
+  }
+
+  const handleChangeThemeText = () => (e: any) => setTheme(e.target.value);
+
+  const updateTheme = (e: any) => {
+    alert(theme);
+    e.stopPropagation();
+  }
+
+  const stopPropagation = (e: any) => {
+    e.stopPropagation();
+  }
+
   return (
     <div className={styles.item} onClick={openContent}>
       <div className={styles.doneDate}>[2021-11-03]</div>
-      <div className={styles.theme}>テーマ：なぜ0秒思考を毎日やるのか？</div>
+      <div className={styles.theme} >
+        テーマ：
+        {isEdit && <><input type="text" value={theme} onChange={handleChangeThemeText()} onClick={stopPropagation} className={styles.themeText} />
+                    　<i onClick={updateTheme} className="fa fa-edit faa-wrench animated-hover" />
+                    　<i onClick={editTheme} className="fa fa-times faa-wrench animated-hover" /></>}
+        {isEdit || <label className="cursorPointer" onClick={editTheme}>なぜ0秒思考を毎日やるのか？</label>}
+      </div>
       <hr />
       {isOpen &&
         <>
