2654f879009f0f641568328b536913a9b6005b11
Tom-Shumi <tshumiya281@gmail.com>
2021-11-09 22:47:29 +0900

    fix layout

--- src/components/ZeroSecondThinking/Form.tsx
+++ src/components/ZeroSecondThinking/Form.tsx
@@ -9,16 +9,29 @@ const Form: React.FC<FormProps> = (props) => {
 
   return (
     <>
-      <FormBootstrap>
-        <Row>
-          <div className={styles.themeDiv}>
-            テーマ：<FormBootstrap.Control className={styles.textTheme} type="text" />
-          </div>
-          <div className={styles.textDiv}>
-            <FormBootstrap.Control className={styles.textTheme} type="text" />
-          </div>
-        </Row>
-      </FormBootstrap>
+      <div className={styles.formDiv}>
+        <FormBootstrap>
+          <Row>
+            <div className={styles.themeDiv}>
+              テーマ：<FormBootstrap.Control className={styles.textTheme} type="text" />
+            </div>
+            <div className={styles.contentDiv}>
+              <div className={styles.textDiv}>
+                <FormBootstrap.Control className={styles.textContent} type="text" />
+                <Button variant="danger" className="buttonSm marginSide10">＋</Button>
+              </div>
+              <div className={styles.textDiv}>
+                　　[なぜ？]　<FormBootstrap.Control className={styles.textContent} type="text" />
+                <Button variant="danger" className="buttonSm marginSide10">＋</Button>
+              </div>
+              <div className={styles.textDiv}>
+                　　[なぜ？]　<FormBootstrap.Control className={styles.textContent} type="text" />
+                <Button variant="danger" className="buttonSm marginSide10">＋</Button>
+              </div>
+            </div>
+          </Row>
+        </FormBootstrap>
+      </div>
     </>
   );
 }
