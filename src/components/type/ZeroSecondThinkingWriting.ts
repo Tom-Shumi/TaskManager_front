f0137155a515c3d3176a84086b7a7704a359bf35
Tom-Shumi <tshumiya281@gmail.com>
2021-11-10 23:57:58 +0900

    add ZeroSecondThinkingWriting

---
+++ src/components/type/ZeroSecondThinkingWriting.ts
@@ -0,0 +1,9 @@
+export class ZeroSecondThinkingWriting {
+  content: string;
+  why: string[];
+
+  constructor(content: string, why: string[]) {
+    this.content = content;
+    this.why = why;
+  }
+}
