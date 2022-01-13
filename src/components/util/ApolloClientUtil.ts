31a1797e4daab09b56a0e29e4192ac069f72485f
Tom-Shumi <tshumiya281@gmail.com>
2022-01-13 22:04:59 +0900

    mod config graphql URL

--- src/components/util/ApolloClientUtil.ts
+++ src/components/util/ApolloClientUtil.ts
@@ -2,9 +2,11 @@ import { InMemoryCache } from '@apollo/client';
 import { ApolloLink } from '@apollo/client';
 import { ApolloClient } from '@apollo/client';
 import { createHttpLink } from '@apollo/client';
+import getConfig from "next/config";
+const { publicRuntimeConfig }= getConfig();
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:8080/graphql", // TODO config化
+  uri: `${publicRuntimeConfig.NEXT_PUBLIC_API_SERVER}${process.env.NEXT_PUBLIC_API_GRAPHQL}`,
   credentials: 'include'
 });
 
